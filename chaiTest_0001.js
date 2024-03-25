38912f11d64c1f5c5745dfabe556c9d7fa3ccd34
s5.islam <s5.islam@a1qa.com>
2024-03-25 20:07:58 +0600

    chaiTest_0001.js is not running

--- chaiTest_0001.js
+++ chaiTest_0001.js
@@ -2,23 +2,24 @@
 
 const assert=require(`chai`).assert;
 const expect=require(`chai`);
+// const{assert,expect}=require(`chai`);
 
 
 describe(`Array`,function(){
 
 	describe(`#indexOf()`,function(){
 
-		const arr1=[6,3,8];
-
 		it(`should return -1 when the value is not present`,function(){
+			const arr1=[6,3,8];
 			const result=arr1.indexOf(10);
 			assert.equal(result,-1,"Expected value is not present");
 		});
 
 
 		it(`should return 1 when the value is present`,function(){
+			const arr1=[6,3,8];
 			const result=arr1.indexOf(8);
-			assert.equal(result,-1,"Expected value is present");
+			expect(result).to.equal(1,`Expected index to be 1`);
 		});
 
 	});
