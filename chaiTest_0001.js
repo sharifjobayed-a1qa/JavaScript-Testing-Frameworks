4406e90899f369bea2eb7374cdf88d1ae3a6d2a8
s5.islam <s5.islam@a1qa.com>
2024-03-25 20:55:05 +0600

    chaiTest_0001.js fixed

--- chaiTest_0001.js
+++ chaiTest_0001.js
@@ -2,7 +2,7 @@
 
 // const assert=require(`chai`).assert;
 // const expect=require(`chai`);
-import { assert, expect } from `chai`;
+import { assert, expect } from 'chai';
 
 
 describe(`Array`,function(){
